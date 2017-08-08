a8d322a0f916ff501e98e0ccfef3103c213f7a94
l@$T <lastiswhat@gmail.com>
2017-08-08 11:13:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
+$q = $_GET['q'].' movie';
+$a = file_get_contents("https://www.google.co.in/search?site=&source=hp&q=".urlencode($q));
+$classname = "_B5d";
 
-echo 'Hello World!';
+$internalErrors = libxml_use_internal_errors(true);
+    $domdocument = new DOMDocument();
+    $domdocument->loadHTML($a);
+    $a = new DOMXPath($domdocument);
+    $spans = $a->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+
+    for ($i = $spans->length - 1; $i > -1; $i--) {
+        $result[] = $spans->item($i)->firstChild->nodeValue;
+    }
+    if (!isset($result)) {
+    	$result = ['N/A'];
+    }
+    echo $result[0];
